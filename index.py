a75eec7063976dd51ae7be3d2ed126c0835c6c92
Rohan Mathur <rmathur101@gmail.com>
2017-12-09 14:39:08 -0600

    clinic page

--- index.py
+++ index.py
@@ -1,7 +1,24 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, request
 app = Flask(__name__)
+app.debug = True
 
 # url_for('static', filename='index.html')
 @app.route('/')
 def root():
 	return render_template("index.html")
+
+@app.route('/clinic')
+def clinic():
+	return render_template("clinic.html")
+
+@app.route("/generate_voucher", methods=["POST"])
+def generate_voucher():
+	print request.form
+	return "success"
+
+def ccc_admin():
+	return
+
+def ride_austin_admin():
+	return
+
